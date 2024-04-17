4d0eccc96e4f128daf18cbdb889f29c1e88d9881
bernardocastro95 <bernardoc544@gmail.com>
2024-04-16 21:29:19 -0400

    Character added and moving

---
+++ RunnerGuy/Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private float speed = 30.0f;
+    public GameObject character;
+    float inputX, inputY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement();
+    }
+
+    void movement()
+    {
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        }
+
+        
+    }
+}
