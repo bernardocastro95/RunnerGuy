418e73f916602066983a0bd79e5430d0f2b40249
bernardocastro95 <bernardoc544@gmail.com>
2024-06-10 21:47:27 -0400

    Randomizer Completed

--- RunnerGuy/Assets/Scripts/RandomizeLevel.cs
+++ RunnerGuy/Assets/Scripts/RandomizeLevel.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RandomizeLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] section;
+    public int xPos = 50;
+    public bool createSection = false;
+    public int secNum;
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(createSection == false)
+        {
+            createSection = true;
+            StartCoroutine(generateSection());
+        } 
+    }
+
+    IEnumerator generateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(section[secNum], new Vector3(xPos, 0,0), Quaternion.identity);
+        xPos -= 50;
+        yield return new WaitForSeconds(2);
+        createSection = false;
     }
 }
