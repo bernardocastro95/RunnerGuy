24ddc0d1ad447ee2ade02fa354e656ff637d5406
bernardocastro95 <bernardoc544@gmail.com>
2024-06-17 23:30:38 -0400

    Ui building and chronometer implemented

---
+++ RunnerGuy/Assets/Scripts/UiManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Text chronometer, score;
+    private float currentTime;
+    private bool timerActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0f;
+        startTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chronometerTimer();
+    }
+    void chronometerTimer()
+    {
+        if (timerActive)
+        {
+            currentTime += Time.deltaTime;
+            TimeSpan time = TimeSpan.FromSeconds(currentTime);
+            chronometer.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+
+        }
+    }
+
+    void startTime()
+    {
+        timerActive = true;
+    }
+    void stopTimer()
+    {
+        timerActive = false;
+    }
+}
